a85f10811374cbf62f8b4eb14023a4e54786e1e3
Ramil <vk2011@bk.ru>
2021-10-06 21:40:55 +0500

    add styled-components and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
+const EmpItem = styled.div`
+	padding: 20px;
+	margin-bottom: 15px;
+	border-radius: 5px;
+	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+	a {
+		display: block;
+		margin: 10px 0 10px 0;
+		color: ${props => props.active ? 'orange' : 'black'};
+	}
+	input {
+		display: block;
+		margin-top: 10px;
+	}
+`;
+
+const Header = styled.h2`
+	font-size: 22px;
+`;
+
+export const Button = styled.button`
+	display: block;
+	padding: 5px 15px;
+	background-color: gold;
+	border: 1px solid  rgba(0, 0, 0, .2);
+	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+	cursor: pointer;
+`;
+
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++',
+			position: 0
+		}
+	}
+
+	nextYear = () => {
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+	commitInputChanges = (e, color) => {
+		console.log(color)
+		this.setState({
+			position: e.target.value
+		})
+	}
+
+	render() {
+		const {name, surname, link} = this.props;
+		const {years, text, position} = this.state;
+		return (
+			<EmpItem active>
+				<Button onClick={this.nextYear}>{text}</Button>
+				<Header>My name is {name}, surname - {surname}, age - {years}, position - {position}</Header>
+				<a href={link}>My Link</a>
+				<form>
+					<span>Введите должность</span>
+					<input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+				</form>
+			</EmpItem>
+		)
+	}
+}
+
+const Wrapper = styled.div`
+	width: 600px;
+	margin: 80px auto 0 auto;
+
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<WhoAmI name="Diana" surname="Altdinova" link="vk.com"/>
+			<WhoAmI name="Ramil" surname="Galikeev" link="facebook.com"/>
+		</Wrapper>
+	);
 }
 
 export default App;
