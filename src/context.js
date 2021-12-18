bb278c8b188b8bfb749f1705ebf400ba7b99a714
Рамиль Галикеев <ramilgalikeev@MacBook-Air-Ramil.local>
2021-12-18 18:01:28 +0500

    fixed

--- src/context.js
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react';
-
-const dataContext = createContext({
-    mail: "name@example.com",
-    text: 'some text',
-    forceChangeMail: () => {}
-});
-
-export default dataContext;
